bd237fb05b156aad01351872bfb55596982c3f70
Atman Dhol <atmandhol@hotmail.com>
2016-04-28 00:20:17 -0400

    commit-main_py

--- main_py.py
+++ main_py.py
@@ -9,14 +9,14 @@ for line in person:
     line_data = line.split(',')
     graph.create(Node("Person", name=line_data[0], gender=line_data[1], country=line_data[2]))'''
 
-#Fetching, Cleaning, and Storing User Names in a List
+# Fetching, Cleaning, and Storing User Names in a List
 fh1 = open("Data/Names.txt", "r")
 nameslist = []
 for line in fh1:
     cleanline = line.strip()
     nameslist.append(cleanline)
 
-#Getting the AgeList for each User
+# Getting the AgeList for each User
 agelist = []
 for i in range(0,250):
     agelist.append(random.randint(15,20))
@@ -28,7 +28,7 @@ for i in range(0,50):
     agelist.append(random.randint(41,50))
 random.shuffle(agelist)
 
-#Fetching the Gender of every User
+# Fetching the Gender of every User
 genderlist = []
 for i in range(0, 1000):
     if random.randint(1,2) == 1:
@@ -36,7 +36,7 @@ for i in range(0, 1000):
     else:
         genderlist.append('F')
 
-#Getting the Nationality of every User
+# Getting the Nationality of every User
 country = ["US", "JAPAN", "FRANCE", "PORTUGAL", "CHILE", "TAIWAN", "SOUTH KOREA", "ITALY", "UAE",
                "COSTA RICA", "SLOVENIA", "BELGIUM", "SPAIN", "SINGAPORE", "UK", "GERMANY", "AUSTRIA", "CANADA",
                "IRELAND", "FINLAND", "NETHERLANDS", "NEW ZEALAND", "QATAR", "SWITZERLAND", "LUXEMBOURG",
